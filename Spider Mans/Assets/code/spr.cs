39cc32432fb07be460e7da586833b7bcfcf37180
Apexvixen <114736175+Apexvixen@users.noreply.github.com>
2023-11-23 13:11:29 +0100

    wasd/walking

--- Spider Mans/Assets/code/spr.cs
+++ Spider Mans/Assets/code/spr.cs
@@ -6,7 +6,7 @@ public class spr : MonoBehaviour
 {
     public bool ismoving = false;
     public float movementspeedsprint = 200;
-    public float movementspeed = 200;
+    
     
 
     // Update is called once per frame
